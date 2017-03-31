95e19b69df382218c52069de1fa9e0b3c438ccaf
Ken Prayogo <ken.prayogo@zomia.org>
2017-03-30 17:43:24 -0700

    Initialize more firebase settings Add BaseActivity class for others to extend from

---
+++ Android-WardrobeApp/app/src/main/java/edu/northeastern/wardrobeapp/android_wardrobeapp/BaseActivity.java
@@ -0,0 +1,80 @@
+package edu.northeastern.wardrobeapp.android_wardrobeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected FirebaseAuth mAuth;
+    protected FirebaseAuth.AuthStateListener mAuthListener;
+    protected String userId;
+    private StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    startActivity(new Intent(BaseActivity.this, LoginActivity.class));
+                }
+            }
+        };
+        // User
+        userId = mAuth.getCurrentUser().getUid();
+        // Storage
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void SignOut() {
+        mAuth.signOut();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_sign_out:
+                SignOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
