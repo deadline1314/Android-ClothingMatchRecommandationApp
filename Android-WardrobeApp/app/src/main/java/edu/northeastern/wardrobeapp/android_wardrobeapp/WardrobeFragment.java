f2e986eafae5012ddd68ef7c9981b1695502123c
Ruotian(Kerwin) Wang <wang.ruot@husky.neu.edu>
2017-03-30 17:38:40 -0700

    fragment adj for wardrobe

---
+++ Android-WardrobeApp/app/src/main/java/edu/northeastern/wardrobeapp/android_wardrobeapp/WardrobeFragment.java
@@ -0,0 +1,22 @@
+package edu.northeastern.wardrobeapp.android_wardrobeapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WardrobeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_wardrobe, container, false);
+        return rootView;
+    }
+}
