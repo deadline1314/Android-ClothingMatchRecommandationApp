d476755fd18114b88371e73ec4f19efe7abd9b7e
leineu2016 <zhou.l@husky.neu.edu>
2017-04-01 01:33:45 -0700

    Login

--- Android-WardrobeApp/app/src/main/java/edu/northeastern/wardrobeapp/android_wardrobeapp/ClothingDetailedViewActivity.java
+++ Android-WardrobeApp/app/src/main/java/edu/northeastern/wardrobeapp/android_wardrobeapp/ClothingDetailedViewActivity.java
@@ -1,13 +1,27 @@
 package edu.northeastern.wardrobeapp.android_wardrobeapp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.Intent;
 
 public class ClothingDetailedViewActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button save;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_detailed_view);
+        imageView = (ImageView) findViewById(R.id.imageViewSmall);
+        save = (Button) findViewById(R.id.saveDetails);
+        Intent showIntent = getIntent();
+        Bitmap bitmap = (Bitmap) showIntent.getParcelableExtra("bitmap");
+        imageView.setImageBitmap(bitmap);
+
     }
 }
